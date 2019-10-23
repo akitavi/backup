208de7937ba044944f949c19e51c40b0fd2b98ad
akit <akit@debian-home>
2019-10-24 00:28:29 +0300

    first step

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+DBNAME='home'
+NOW=$(date +%d.%m.%Y)
+FULLNAME=db\_$DBNAME\_$NOW.sql
+USER='test'
+PASSWD='test'
+NUMBER="32"
+STRING="Its a var"
+TEXT="<i>Новых заметок: </i> <br>
+ <i>Всего заметок: </i> <br>
+ <i>Попыток входа на сайт: </i> <br>
+ <b>Бэк ап во вложении</b>"
+
+
+mysqldump -u $USER -p$PASSWD $DBNAME > ./$FULLNAME 2>/dev/null
+zip $FULLNAME.zip $FULLNAME && \
+echo $TEXT | mail -a 'Content-type: text/html; charset="UTF8"' -s "Report for $NOW" -aFrom:Envi\ System\<groov6000@gmail.com\> apalse@yandex.ru  && \
+rm $FULLNAME && mv $FULLNAME.zip ./archive
